16dcb33a8499b9dc4792afbb815ca8e3f26395a6
Juan Bollero <BolleroJuanIgnacio1240@gmail.com>
2022-05-18 16:48:54 -0300

    Added new datepicker package for using on forms, added css for datepicker and upadted theme with font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,29 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'encode-sans': ['Encode Sans', 'sans-serif'],
+    },
     extend: {
       minWidth: {
         'qualification-options': '6.875rem',
       },
       maxWidth: {
         'student-form-section': '48rem',
+      },
+      width: {
+        147: '36.75rem',
+      },
+      colors: {
+        primary: {
+          500: '#37BBED',
+        },
+        error: {
+          500: '#F4626B',
+        },
+        success: {
+          500: '#1BBB1F',
+        },
       },
     },
   },
